479061b321dfc51d57dad24fb9be53dfaa42ee48
Steeven Bossé <steeven.bosse@usherbrooke.ca>
2018-10-28 17:36:12 -0400

    Link server to database

--- server/coach_player_server/settings.py
+++ server/coach_player_server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'coach_player_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coach-player-dev',
+        'USER': 'coachadmin',
+        'PASSWORD': 'coach1234',
+        'HOST': 'coach-player-postgres',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
